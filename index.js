e1eb5d340a5b2e0e4313db4819af28de4d5af145
Jessica Campos <Jessica.c@bronxsoftware.org>
2023-05-26 13:43:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+/*const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];*/
+
+// returns the matching list of drivers. The function should be case insensitive.
+function findMatching(drivers, string) {
+    const match = drivers.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+    return match;
+}
+
+// returns all drivers whose names begin with the provided letters.
+function fuzzyMatch(drivers, string) {
+    const match = drivers.filter(function (driver) {
+        return driver.substring(0, string.length) === string;
+    });
+    return match;
+}
+
+// return each element whose name property matches the provided string argument.
+function matchName(drivers, name) {
+    const match = drivers.filter(function (driver) {
+        return driver.name === name;
+    });
+    return match;
+}
